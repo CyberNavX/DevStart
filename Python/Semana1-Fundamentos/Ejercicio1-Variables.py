a2cc8c70cd706d8ae47a0c987587459a2f087130
CyberNavX <gerbergarciasalas52@gmail.com>
2025-09-24 13:05:57 -0600

    Avance en Python

---
+++ Python/Semana1-Fundamentos/Ejercicio1-Variables.py
@@ -0,0 +1,17 @@
+#      Semana1-Fundamentos/Ejercicio1_Variables
+#
+#      #### Variables #### 
+#
+#  Las variables son espacios en la memoria que guarda un valor y tiene un nombre que lo identifica.
+#  En Python las variables no se necesita indicar el tipo de dato, solo se le da valor que se necesite a la variable. 
+
+print(" ### Variables ###\n")
+
+numero = 34
+texto = "Hola mundo"
+puntoDecimal = 4.1416
+
+print(f"Mi primer programaes: {texto}")
+print(f"Este es un numero entero: {numero}")
+print(f"Este es un numero con punto decimal: {puntoDecimal}")
+
